3962f652ff93169a7df2647a510389def5fde446
vism <vismiroglou@gmail.com>
2023-10-13 10:24:10 +0200

    Added annotation reading to DF

--- src/dataset.py
+++ src/dataset.py
@@ -2,24 +2,29 @@ import os
 import pandas as pd
 import torch
 import cv2 as cv
+from glob import glob
 
-class thermal_data(torch.utils.data.Dataset):
-    def __init__(self, annotations_dir, transform=None, target_transform=None):
-        self.img_labels = 
-        self.clip_dir = 
+class FrameDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transform=None, target_transform=None):
+        self.root = root
         self.transform = transform
         self.target_transform = target_transform
+        self.img_files = sorted(glob(os.path.join(root, 'clips' + '*/*/*/*.jpg')))
+        self.columns = ['id', 'class','x1','y1','x2','y2']
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.img_files)
 
     def __getitem__(self, idx):
-        frame = cv.imread(self.clip_dir)
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        img_path = self.img_files[idx]
+        annotations_path = img_path.replace('clips', 'annotations').replace('.jpg', '.txt')
+        frame = cv.imread(img_path)
+        annos = pd.read_csv(annotations_path, names = self.columns, delimiter=' ', usecols= range(6))
+
+
+
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        return frame, annos
