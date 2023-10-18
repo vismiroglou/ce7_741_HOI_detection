428982e0cd3fbe05b1e93dae07da547422ab1166
vismiroglou <vismiroglou@gmail.com>
2023-10-18 13:31:04 +0200

    Added data and a script to extract them

---
+++ src/extract_data.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+#data_list as the path to the file
+def extract_data(root, destination, data_list):
+    with open(data_list, 'r') as f:
+        data = f.read()
+        data = data.split("\n").replace('../data', root)
+
+    for i in data:
+        if os.path.isfile(i):
+            anno_folder_ini = i.replace('clips','annotations').replace('.mp4', '')
+            path_clips = os.path.join(i.split('\\')[0], i.split('\\')[1]).replace(root, destination)
+            path_anno = path_clips.replace('clips', 'annotations')
+
+            if not os.path.exists(path_clips):
+                os.makedirs(path_clips)
+            if not os.path.exists(path_anno):
+                os.makedirs(path_anno)
+            
+            shutil.move(i, path_clips)
+            shutil.move(anno_folder_ini, path_anno)
