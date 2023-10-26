4d0c27f82ce2f332c23e96910fc266ab483e0910
vismiroglou <vismiroglou@gmail.com>
2023-10-26 21:12:56 +0200

    added an existing directory check

--- scripts/makeframes.py
+++ scripts/makeframes.py
@@ -5,19 +5,22 @@ def make_frames(clip_dir):
     pathOut = clip_dir.replace('.mp4','')
     if not os.path.exists(pathOut):
         os.makedirs(pathOut)
-    vidcap = cv.VideoCapture(clip_dir)
-    count = 1
-    success = True
-    while success:
-        success,image = vidcap.read()
-        if count%25 == 0:
-            frame_path = os.path.join(pathOut, 'frame_%s.jpg'%'{0:04}'.format(int(count/25)))
-            anno_path = frame_path.replace('clips', 'annotations').replace('frame', 'annotations').replace('.jpg', '.txt')
-            if os.path.exists(anno_path):
-                cv.imwrite(frame_path, image)
-            else:
-                print('Annotation for', frame_path, 'not found')
-        count+=1
+    
+        vidcap = cv.VideoCapture(clip_dir)
+        count = 1
+        success = True
+        while success:
+            success,image = vidcap.read()
+            if count%25 == 0:
+                frame_path = os.path.join(pathOut, 'frame_%s.jpg'%'{0:04}'.format(int(count/25)))
+                anno_path = frame_path.replace('clips', 'annotations').replace('frame', 'annotations').replace('.jpg', '.txt')
+                if os.path.exists(anno_path):
+                    cv.imwrite(frame_path, image)
+                else:
+                    print('Annotation for', frame_path, 'not found')
+            count+=1
+    else:
+        print('Video', clip_dir, 'has already been extracted.')
 
 
 if __name__ == '__main__':
