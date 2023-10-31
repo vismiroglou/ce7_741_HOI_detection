7b1a32dd28eafa2bb502c5516f4bec4a31fbcc92
vismiroglou <vismiroglou@gmail.com>
2023-10-31 10:04:17 +0100

    fix: works on empty anno files

--- scripts/create_1fps_anno_videos.py
+++ scripts/create_1fps_anno_videos.py
@@ -8,36 +8,37 @@ import re
 def make_videos(root):
     size = (384, 288)
 
-    for video in glob(os.path.join(root, '*/*.mp4')):
+    for video in glob(os.path.join(root, '*.mp4')):
         frame_dir = video.replace('.mp4', '')
         video_name = video.split('\\')[-1]
         if os.path.isdir(frame_dir):
             img_array=[]
             for frame in glob(os.path.join(frame_dir, '*.jpg')):
                 anno_path = frame.replace('clips', 'annotations').replace('frame', 'annotations').replace('.jpg', '.txt') 
+                img = cv2.imread(frame)
+                #Try to read annotations for the frame
                 try:
                     data = pd.read_csv(anno_path, sep=' ', header=None)
+                    for i in data.index:
+                        x1 = data.loc[i, 2]
+                        y1 = data.loc[i, 3]
+                        x2 = data.loc[i, 4]
+                        y2 = data.loc[i, 5]
+                        if data.loc[i, 1] == 'human':
+                            color = (0,255,0)
+                        elif data.loc[i, 1] == 'bicycle':
+                            color = (255,0,0)
+                        elif data.loc[i, 1] == 'motorcycle':
+                            color = (0, 0, 255)
+                        else:
+                            color = (255, 255, 255)
+
+                        img = cv2.rectangle(img,(x1,y1),(x2,y2), color, 1)
+                        img = cv2.putText(img, str(data.loc[i, 0]), org=(x1,y1), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=color)
                 except:
                     print('No annotations')
-                img = cv2.imread(frame)
-                for i in data.index:
-                    x1 = data.loc[i, 2]
-                    y1 = data.loc[i, 3]
-                    x2 = data.loc[i, 4]
-                    y2 = data.loc[i, 5]
-                    if data.loc[i, 1] == 'human':
-                        color = (0,255,0)
-                    elif data.loc[i, 1] == 'bicycle':
-                        color = (255,0,0)
-                    elif data.loc[i, 1] == 'motorcycle':
-                        color = (0, 0, 255)
-                    else:
-                        color = (255, 255, 255)
 
-                    img = cv2.rectangle(img,(x1,y1),(x2,y2), color, 1)
-                    img = cv2.putText(img, str(data.loc[i, 0]), org=(x1,y1), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=color)
-                    img = cv2.putText(img, str(frame), org=(10, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=(255,255,255))
-                
+                img = cv2.putText(img, str(frame), org=(10, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3, color=(255,255,255))
                 img_array.append(img)
 
             video_out = os.path.join(frame_dir, '%s.avi'%video_name.replace('.mp4', ''))
@@ -52,4 +53,4 @@ def make_videos(root):
 
 if __name__ == '__main__':
 
-    make_videos('../../data_inter/clips')
+    make_videos('../../data_inter/clips/20200515')
